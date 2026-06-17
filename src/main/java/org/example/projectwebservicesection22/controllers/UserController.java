9460e838b22388b6225c78f159040104cf01fa5c
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2026-06-16 22:15:22 -0300

    Implementing service layer for  user

--- src/main/java/org/example/projectwebservicesection22/controllers/UserController.java
+++ src/main/java/org/example/projectwebservicesection22/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,12 @@ public class UserController {
         List<User> users = userService.findAll();
 
         return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable(value = "id") Long id) {
+        User user = userService.findById(id);
+
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
--- src/main/java/org/example/projectwebservicesection22/services/UserService.java
+++ src/main/java/org/example/projectwebservicesection22/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +16,11 @@ public class UserService {
 
     public List<User> findAll(){
         return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> user = userRepository.findById(id);
+
+        return user.get();
     }
 }
