c626041312c1fa38fae5839c7cce5fcbd9688f13
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2026-06-16 22:28:17 -0300

    Implementing order entity and its realations

---
+++ src/main/java/org/example/projectwebservicesection22/entities/Order.java
@@ -0,0 +1,75 @@
+package org.example.projectwebservicesection22.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant instant;
+//    private OrderStatus orderStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    public Order() {
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void seClient(User user) {
+        this.client = user;
+    }
+
+//    public OrderStatus getOrderStatus() {
+//        return orderStatus;
+//    }
+//
+//    public void setOrderStatus(OrderStatus orderStatus) {
+//        this.orderStatus = orderStatus;
+//    }
+
+    public Instant getInstant() {
+        return instant;
+    }
+
+    public void setInstant(Instant instant) {
+        this.instant = instant;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Order(Long id, Instant instant, User user) {
+        this.id = id;
+        this.instant = instant;
+//        this.orderStatus = orderStatus;
+        this.client = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
--- src/main/java/org/example/projectwebservicesection22/entities/User.java
+++ src/main/java/org/example/projectwebservicesection22/entities/User.java
@@ -3,6 +3,8 @@ package org.example.projectwebservicesection22.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -15,6 +17,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -77,5 +82,9 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 }
