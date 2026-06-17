e53788165f6733d3acbcb7dd755676542cacb076
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2026-06-16 21:33:07 -0300

    Initial commit

---
+++ src/main/java/org/example/projectwebservicesection22/ProjectWebserviceSection22Application.java
@@ -0,0 +1,13 @@
+package org.example.projectwebservicesection22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWebserviceSection22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectWebserviceSection22Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/projectwebservicesection22/ProjectWebserviceSection22ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projectwebservicesection22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWebserviceSection22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
