d30dbdb2c73be7c0efbc1ad32cb8e255d98e9d45
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2026-06-26 02:27:56 -0300

    Implementing ManyToMany associations between product and category

--- src/main/java/org/example/projectwebservicesection22/entities/Category.java
+++ src/main/java/org/example/projectwebservicesection22/entities/Category.java
@@ -1,5 +1,6 @@
 package org.example.projectwebservicesection22.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,6 +17,8 @@ public class Category implements Serializable {
     private Long id;
     private String categoryName;
 
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
