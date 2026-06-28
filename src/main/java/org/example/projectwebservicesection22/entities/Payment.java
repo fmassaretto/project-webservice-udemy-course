2f6d8f23ab42c17bfb0bd351cd527f90fa3733f3
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2026-06-28 03:21:55 -0300

    Implementing Payment entity and associations OneToOne

---
+++ src/main/java/org/example/projectwebservicesection22/entities/Payment.java
@@ -0,0 +1,66 @@
+package org.example.projectwebservicesection22.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant paymentDate;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment(Long id, Instant paymentDate, Order order) {
+        this.id = id;
+        this.paymentDate = paymentDate;
+        this.order = order;
+    }
+
+    public Payment() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Instant paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Payment payment = (Payment) o;
+        return Objects.equals(id, payment.id) && Objects.equals(paymentDate, payment.paymentDate) && Objects.equals(order, payment.order);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, paymentDate, order);
+    }
+}
